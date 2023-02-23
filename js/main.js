8161afb2759e7a931a6b11e698d569d5f82ee269
Dominic <DominicTheDev89@gmail.com>
2023-02-22 22:42:03 -0500

    Set up some variables that will hold dates and times to start and count down to

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+let countDownDate = new Date("Nov 12, 2023 00:00:00").getTime();
+const startTime = new Date("Feb 22, 2023 22:37:00").getTime();
+let now = new Date().getTime();
+let timeLeft = countDownDate - now;
 
+let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+
+let secCounter = setInterval(function() {
+
+}, 1000);
